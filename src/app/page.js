2e499fcb7f26702701246dd4a4f8c33e1c298d6a
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-07-23 14:32:47 +0600

    Marquee added
    added marquee and fixed the color and all

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { GoArrowRight } from "react-icons/go";
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
 
   return (
     <div>
+
       <section className="hero min-h-[70vh] bg-linear-to-br from-red-500 to-amber-900">
         <div className="hero-content text-center">
           <div className="max-w-2xl">
@@ -20,12 +22,17 @@ export default function Home() {
 
             <div className="mt-8">
               <Link href="/books" className="btn border-0 text-xl bg-white normal-case">
-                <span className='bg-linear-to-r from-red-500 to-amber-900 bg-clip-text text-transparent flex justify-around items-center'>Browse Now <GoArrowRight className="ml-2" size={30} color="oklch(47% 0.157 37.304)"/></span>
+                <span className='bg-linear-to-r from-red-500 to-amber-900 bg-clip-text text-transparent flex justify-around items-center'>Browse Now <GoArrowRight className="ml-2" size={30} color="oklch(47% 0.157 37.304)" /></span>
               </Link>
             </div>
           </div>
         </div>
       </section>
+      <div className="bg-amber-500 px-30 py-3 text-amber-900 text-[16px] font-semibold">
+        <Marquee speed={50} gradient={false} pauseOnHover={true}>
+          New Arrivals: JavaScript Mastery | Special Discount on Memberships...
+        </Marquee>
+      </div>
     </div>
   );
 }
