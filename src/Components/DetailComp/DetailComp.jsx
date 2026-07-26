dd4a2f6d8db987f04e5849a7310360df09560772
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-07-26 15:37:22 +0600

    detail page updated

--- src/Components/DetailComp/DetailComp.jsx
+++ src/Components/DetailComp/DetailComp.jsx
@@ -1,8 +1,27 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import { useContext } from 'react';
 import { allContext } from '../../Context/Context';
+
+// {
+//     "id": 1,
+//     "title": "The Lost Kingdom",
+//     "author": "Emma Carter",
+//     "description": "An adventurous tale of a young explorer uncovering the secrets of a forgotten kingdom.",
+//     "category": "Story",
+//     "available_quantity": 12,
+//     "image_url": "https://picsum.photos/seed/book1/400/600"
+//   },
+
+// Visuals: Large Book Cover on the left, text/details on the right.
+// Info: Title, Author, Description, and Available Quantity (e.g., "5 copies left").
+// The Action Button:
+// "Borrow This Book" Button:
+// If Logged Out: Redirects to the Login page.
+// Show a confirmation toast.
+
 
 const DetailComp = ({ bookId }) => {
     const { books } = useContext(allContext);
@@ -13,14 +32,20 @@ const DetailComp = ({ bookId }) => {
     return (
         <div>
             {targetedBook && (
-                <div>
-                    <h2>{targetedBook.title}</h2>
-                    <p>{targetedBook.description}</p>
-                    <p>Author: {targetedBook.author}</p>
-                </div>
+                <div className="flex items-center justify-between my-10 mx-20 p-5 rounded-md shadow-sm bg-amber-50">
+                    <div>
+                        <Image src={targetedBook.image_url} alt={targetedBook.title} width={400} height={600} />
+                    </div>
+                        <div>
+                            <h2>{targetedBook.title}</h2>
+                            <p>{targetedBook.description}</p>
+                            <p>Author: {targetedBook.author}</p>
+                            <p>Available Quantity: {targetedBook.available_quantity}</p>
+                        </div>
+                    </div>
             )}
-        </div>
-    );
+                </div>
+            );
 };
 
-export default DetailComp;
+            export default DetailComp;
