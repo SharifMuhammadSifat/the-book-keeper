7dbad1af614e2c3d7a96b349e392e280a47c52b4
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-07-30 14:37:20 +0600

    removed demo json from detail page

--- src/Components/DetailComp/DetailComp.jsx
+++ src/Components/DetailComp/DetailComp.jsx
@@ -6,22 +6,6 @@ import { useContext } from 'react';
 import { allContext } from '../../Context/Context';
 import { toast } from 'react-toastify';
 
-// {
-//     "id": 1,
-//     "title": "The Lost Kingdom",
-//     "author": "Emma Carter",
-//     "description": "An adventurous tale of a young explorer uncovering the secrets of a forgotten kingdom.",
-//     "category": "Story",
-//     "available_quantity": 12,
-//     "image_url": "https://picsum.photos/seed/book1/400/600"
-//   },
-
-// Visuals: Large Book Cover on the left, text/details on the right.
-// Info: Title, Author, Description, and Available Quantity (e.g., "5 copies left").
-// The Action Button:
-// "Borrow This Book" Button:
-// If Logged Out: Redirects to the Login page.
-// Show a confirmation toast.
 
 
 const DetailComp = ({ bookId }) => {
