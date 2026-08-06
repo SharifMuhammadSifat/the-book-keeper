7addf5995adc3d3b8716ad11f2e781c9a01c9c95
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-08-06 16:50:59 +0600

    created path and dumy page for photo and name update

---
+++ src/app/profile/update-name/page.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const updateName = () => {
+    return (
+        <div>
+            name update page
+        </div>
+    );
+};
+
+export default updateName;
---
+++ src/app/profile/update-picture/page.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const updatePicture = () => {
+    return (
+        <div>
+            picture update page
+        </div>
+    );
+};
+
+export default updatePicture;
