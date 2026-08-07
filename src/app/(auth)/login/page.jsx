6c86da1f6a558a905790c9025d41dbe49caa2100
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-08-07 16:12:20 +0600

    log in and reg forms set up

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import Link from 'next/link';
 const login = () => {
     return (
-        <div>
-            this is login page
+        <div className='flex flex-col items-center justify-center gap-4'>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend">Login</legend>
+
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+            <div className='flex item-center justify-center gap-2'>
+                <h1>Don't have an account?</h1>
+                <Link href="/register" className="btn btn-neutral mt-4">Register</Link>
+            </div>
         </div>
     );
 };
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-
-const register = () => {
+import Link from 'next/link';
+const Register = () => {
     return (
-        <div>
-            this is register page
+        <div className='flex flex-col items-center justify-center gap-4'>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend">Register</legend>
+
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+
+                <label className="label">Photo URL</label>
+                <input type="url" className="input" placeholder="Photo URL" />
+
+                <button className="btn btn-neutral mt-4">Register</button>
+            </fieldset>
+            <div className='flex item-center justify-center gap-2'>
+                <h1>Already have an account?</h1>
+                <Link href="/login" className="btn btn-neutral mt-4">Login</Link>
+            </div>
         </div>
+
+
     );
 };
 
-export default register;
+export default Register;
