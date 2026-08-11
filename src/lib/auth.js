647ac9b6072dc213981c30269c2083c1ed4a7dda
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-08-11 19:36:23 +0600

    edit in env and auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("process.env.MONGODB_URI");
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
@@ -10,7 +10,7 @@ export const auth = betterAuth({
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
+  emailAndPassword: {
+    enabled: true,
+  },
 });
