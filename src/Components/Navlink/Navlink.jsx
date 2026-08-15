fc057b53613429065950b0f2e98db0ed6c2dd995
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-08-15 19:25:12 +0600

    Update Navlink.jsx

--- src/Components/Navlink/Navlink.jsx
+++ src/Components/Navlink/Navlink.jsx
@@ -7,6 +7,8 @@ import { usePathname } from 'next/navigation';
 const Navlink = ({ href, children }) => {
     const pathname = usePathname();
 
+
+
     return (
         <Link href={href} className={`text-black text-xl font-semibold ${pathname === href ? 'border-b-2 border-amber-900 rounded-b-none' : 'hover:bg-linear-to-r from-red-500 to-amber-900 hover:text-white'} p-1.5 rounded-sm`}>
             {children}
